5632a9397f166018aa46ba81c18dfaddd759e89c
fyan0125 <elainefang2001@gmail.com>
2021-03-11 14:36:58 +0800

    first commit

---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+export default function Menu() {
+    return (
+        <menu className="menu text-light opacity-3">
+            <div className="menu-content"><p>Tableware</p></div>
+            <div className="menu-content"><p>Cookware</p></div>
+            <div className="menu-content"><p>Home accessoreies</p></div>
+            <div className="menu-content"><p>Lighting</p></div>
+            <div className="menu-content"><p>Textile</p></div>
+            <div className="menu-content"><p>Furniture</p></div>
+        </menu>
+    );
+}
